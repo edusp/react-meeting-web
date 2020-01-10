ac81174162ac569d19cc47c644a0bee6d7f2c601
edpereira <edpereira@ebay.com>
2020-01-11 10:26:54 +1100

    To deploy

---
+++ src/configuration/config.js
@@ -0,0 +1,14 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyBKzX4NySMINtpoqAe4v81t34Gj-abhuGo",
+        authDomain: "meeting-react-app.firebaseapp.com",
+        databaseURL: "https://meeting-react-app.firebaseio.com",
+        projectId: "meeting-react-app",
+        storageBucket: "meeting-react-app.appspot.com",
+        messagingSenderId: "272771970019",
+        appId: "1:272771970019:web:a82186892cea00f12d26d6"
+    }
+
+}
+
+export default config;
